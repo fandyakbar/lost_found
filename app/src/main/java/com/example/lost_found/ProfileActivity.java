9990ddfda7e5dc1f1df3535f3f36080e79827b26
Hasya Rona Amirahmi <hasyarona1711@gmail.com>
2021-11-27 15:36:13 +0700

    isi class profileactivity

--- app/src/main/java/com/example/lost_found/ProfileActivity.java
+++ app/src/main/java/com/example/lost_found/ProfileActivity.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfileActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class ProfileActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
